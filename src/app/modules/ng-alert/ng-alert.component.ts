8556746c6f6184819c668884cb3ccaf7e9d71243
Theophilus Omorebee <theo4u@ymail.com>
2018-01-24 22:22:10 +0100

    feat: init project

---
+++ src/app/modules/ng-alert/ng-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-alert',
+  templateUrl: './ng-alert.component.html',
+  styleUrls: ['./ng-alert.component.css']
+})
+export class NgAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/modules/ng-alert/ng-alert.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgAlertComponent } from './ng-alert.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [NgAlertComponent]
+})
+export class NgAlertModule { }
