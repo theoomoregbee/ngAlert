e3602a5bd775476a8f4210a1b3921b136d92f9f1
Theophilus Omorebee <theo4u@ymail.com>
2018-01-25 10:02:02 +0100

    chore: expose interfaces

--- public_api.ts
+++ public_api.ts
@@ -1,1 +1,3 @@
-export * from './src/app/modules/ng-alert/ng-alert.module'
+export * from './src/app/modules/ng-alert/ng-alert.module';
+export { NgAlertService } from './src/app/modules/ng-alert/ng-alert.service';
+export { IMessage, CloseType, MessageType } from './src/app/modules/ng-alert/ng-alert.component';
