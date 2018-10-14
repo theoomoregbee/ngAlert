6c9113f809b5819b44d1c2f2ddfde33c86fcc53b
Theophilus Omoregbee <theo4u@ymail.com>
2018-10-14 21:05:55 +0100

    chore: init with angular 6 library

---
+++ projects/alert/src/lib/alert.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { AlertComponent } from './alert.component';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [AlertComponent],
+  exports: [AlertComponent]
+})
+export class AlertModule { }
---
+++ projects/alert/src/public_api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of alert
+ */
+
+export * from './lib/alert.service';
+export * from './lib/alert.component';
+export * from './lib/alert.module';
